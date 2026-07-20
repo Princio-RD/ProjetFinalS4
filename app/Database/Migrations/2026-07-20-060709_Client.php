4d2752f0bbcb099e520ed73882e895ebae481774
Iavo1926 <maxrazaf622@gmail.com>
2026-07-20 09:14:50 +0300

    i
    v

---
+++ app/Database/Migrations/2026-07-20-060709_Client.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Client extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_client' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'numero_telephone' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '15',
+                'null'       => false,
+                'unique'     => true,
+            ],
+            'date_creation' => [
+                'type'    => 'DATETIME',
+                'null'    => false,
+                'default' => new \CodeIgniter\Database\RawSql('CURRENT_TIMESTAMP'),
+            ],
+        ]);
+        $this->forge->addKey('id_client', true);
+        $this->forge->createTable('Client');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('Client', true);
+    }
+}
---
+++ app/Database/Migrations/2026-07-20-060711_Transaction.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Transaction extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_transaction' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'id_compte_source' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+                'null'       => false,
+            ],
+            'id_compte_destination' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+                'null'       => true,
+            ],
+            'id_type_operation' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+                'null'       => false,
+            ],
+            'montant' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '15,2',
+                'null'       => false,
+            ],
+            'frais_applique' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '15,2',
+                'null'       => false,
+                'default'    => 0,
+            ],
+            'date_operation' => [
+                'type'    => 'DATETIME',
+                'null'    => false,
+                'default' => new \CodeIgniter\Database\RawSql('CURRENT_TIMESTAMP'),
+            ],
+            'statut' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '20',
+                'null'       => false,
+                'default'    => 'Réussi',
+            ],
+        ]);
+        $this->forge->addKey('id_transaction', true);
+        $this->forge->addForeignKey('id_compte_source', 'Compte', 'id_compte', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('id_compte_destination', 'Compte', 'id_compte', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('id_type_operation', 'Operation', 'id_type_operation', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('Transaction');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('Transaction', true);
+    }
+}
